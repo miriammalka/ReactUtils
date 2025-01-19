188fab5ce8d4e1b3bccefaf05bcad696664f4882
miriammalka <miriammalka1234@gmail.com>
2025-01-19 12:36:21 -0500

    integrated in error handling

--- src/DataUtil.ts
+++ src/DataUtil.ts
@@ -1,37 +1,52 @@
 import { FieldValues } from "react-hook-form";
 
-export function createAPI(baseurl: string) {
+const hasErrorMessage = <T>(data: T): data is T & { errorMessage?: string } => {
+    return (data as { errorMessage?: string }).errorMessage !== undefined;
+};
 
+export function createAPI(baseurl: string, sessionkey: string) {
 
+    async function request<T>(url: string, options: RequestInit): Promise<T> {
+        url = baseurl + url;
+        const response = await fetch(url,
+            {
+                ...options,
+                headers: {
+                    ...options.headers,
+                    "Authorization": `Bearer ${sessionkey}`
+                }
+            }
+        );
+        const data = await response.json();
+        if (!response.ok) {
+            if (response.status == 401) {
+                throw new Error("Unauthorized");
+            }
+            else if (!hasErrorMessage(data) || !data.errorMessage) {
+                throw new Error("error calling the API");
+            }
+        }
+        return data;
+    }
 
     async function fetchData<T>(url: string): Promise<T> {
-        url = baseurl + url;
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
+        return request(url, {});
     }
 
     async function deleteData<T>(url: string): Promise<T> {
-        url = baseurl + url;
-        const response = await fetch(url, {
-            method: "DELETE"
-        });
-        const data = await response.json();
-        return data;
+        return request(url, { method: "DELETE" });
     }
 
     async function postData<T>(url: string, form: FieldValues): Promise<T> {
-        url = baseurl + url;
-        const response = await fetch(url, {
+        return request(url, {
             method: "POST",
             body: JSON.stringify(form),
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             }
         });
-        const data = await response.json();
-        return data;
     };
     return { fetchData, deleteData, postData };
 };
+
 
---
+++ src/Login.tsx
@@ -0,0 +1,52 @@
+import { useForm } from "react-hook-form"
+import { getUserStore } from "./UserStore";
+import { useState } from "react";
+
+type forminput = { username: string, password: string }
+
+export default function Login() {
+    const { register, handleSubmit, formState: { errors } } = useForm<forminput>();
+    const apiurl = "http://localhost:5236/api/";
+    const useUserStore = getUserStore(apiurl);
+    const login = useUserStore((state) => state.login);
+    const logout = useUserStore((state) => state.logout);
+    const username = useUserStore((state) => state.userName);
+    const isLoggedIn = useUserStore(state => state.isLoggedIn);
+    const [crashmsg, setCrashmsg] = useState("");
+    const errormsg = useUserStore(state => state.errorMessage);
+    const onSubmit = async (data: forminput) => {
+        try {
+            setCrashmsg("");
+            await login(data.username, data.password)
+        }
+        catch (error: unknown) {
+            if (error instanceof Error) {
+                setCrashmsg(error.message);
+            }
+            else {
+                setCrashmsg("Error occured");
+            }
+        }
+    };
+
+    return (
+        <>
+            <div>Logged In = {isLoggedIn.toString()}</div>
+            <h2 className="text-danger">{crashmsg || errormsg}</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Username</label>
+                <input type="text" {...register('username', { required: 'Username is required' })} />
+                {errors.username && <span>{errors.username.message}</span>}
+                <br />
+                <label>Password</label>
+                <input type="password"
+                    {...register('password', { required: 'Password is required' })} />
+                {errors.password && <span>{errors.password.message}</span>}
+                <br />
+                <button type="submit">Login</button>
+
+            </form>
+            <button onClick={() => logout(username)}>Logout</button>
+        </>
+    )
+}
